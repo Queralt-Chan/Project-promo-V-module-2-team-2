c415e0f1af7b7a996d5c90757bce16f75c01f6f9
natlopar <natalialopezariel@gmail.com>
2024-01-04 14:05:29 +0100

    object and functions form

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,14 @@ const previewMail = document.querySelector('.js-mailLink');
 const previewLinkedin = document.querySelector('.js-linkedinLink');
 const previewGithub = document.querySelector('.js-githubLink');
 const formContainer = document.querySelector('.js-formContainer');
+const previewCard= document.querySelector('.js-card');
+
+
+//Variables Design
+const palette1= document.querySelector('.js_palette1');
+const palette2= document.querySelector('.js_palette2');
+const palette3= document.querySelector('.js_palette3');
+const formDesign= document.querySelector('.formDesign');
 
 //Variables Fill
 const inputName = document.querySelector('.js_inputName');
@@ -16,24 +24,60 @@ const inputMobile = document.querySelector('.js_inputPhone');
 const inputMail = document.querySelector('.js_inputEmail');
 const inputLinkedin = document.querySelector('.js_inputLinkedin');
 const inputGithub = document.querySelector('.js_inputGithub');
+const form= document.querySelector('.formStyle');
+const data= {
+    palette: 1,
+    name: '',
+    job: '',
+    phone: '',
+    email: '',
+    linkedin: '',
+    github: '',
+    photo: '',
+}
+
 
 function handleForm(event) {
   console.log(event.currentTarget);
   console.log(event.target);
-  if (event.target.id === 'name') {
+  const inputId= event.target.id;
+  if (inputId === 'name') {
+    data.name= event.target.value;
     previewName.innerHTML = inputName.value;
-  } else if (event.target.id === 'job') {
+    console.log(data.name);
+  } else if (inputId === 'job') {
+    data.job= event.target.value;
     previewJob.innerHTML = inputJob.value;
-  } else if (event.target.id === 'phone') {
-    previewMobile.href = inputMobile.value;
-  } else if (event.target.id === 'email') {
-    previewMail.href = inputMail.value;
-  } else if (event.target.id === 'linkedin') {
+  } else if (inputId === 'phone') {
+    data.phone= event.target.value;
+    previewMobile.href = 'tel:' + inputMobile.value;
+  } else if (inputId === 'email') {
+    data.email= event.target.value;
+    previewMail.href = 'mailto:' + inputMail.value;
+  } else if (inputId === 'linkedin') {
+    data.linkedin= event.target.value;
     previewLinkedin.href = inputLinkedin.value;
-  } else if (event.target.id === 'github') {
+  } else if (inputId === 'github') {
+    data.github= event.target.value;
     previewGithub.href = 'https://github.com/' + inputGithub.value;
   }
 };
+
+function handleDesign(event) {
+    previewCard.classList.remove('palette1', 'palette2', 'palette3');
+    if (palette1.checked===true) {
+        previewCard.classList.add('palette1');
+    } else if (palette2.checked===true) {
+        previewCard.classList.add('palette2');
+    } else  {
+        previewCard.classList.add('palette3');
+    }
+};
+
+
+
 //campos de los links (@ github, ayuda linkedin, mail y phone)
 
 formContainer.addEventListener('input', handleForm);
+
+formDesign.addEventListener('input', handleDesign);
