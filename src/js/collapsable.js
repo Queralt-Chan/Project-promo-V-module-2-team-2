4db498f05bdcf123270734a4198b2795d79826af
Mbueno992 <mbueno_92@hotmail.com>
2024-01-10 13:20:25 +0100

    add collapsable, reset data, checked

--- src/js/collapsable.js
+++ src/js/collapsable.js
@@ -1,16 +1,56 @@
 'use strict';
 
+const mainOption = document.querySelectorAll('.js-options');
+const fill = document.querySelector('.js-fill');
+const design = document.querySelector('.js-design');
+const share = document.querySelector('.js-share');
+const fieldsetDesign = document.querySelector('.js-fieldsetDesign');
+const fieldsetFill = document.querySelector('.js-fieldsetFill');
+const fieldsetShare = document.querySelector('.js-fieldsetShare');
 
-// const option = document.querySelectorAll('.options');
+const fillOption = () => {
+  if (fieldsetFill.classList.contains('hidden')) {
+    fieldsetDesign.classList.add('hidden');
+    fieldsetFill.classList.remove('hidden');
+    fieldsetShare.classList.add('hidden');
+  } else {
+    fieldsetFill.classList.add('hidden');
+    fieldsetShare.classList.remove('hidden');
+  }
+};
 
-// function handleClickFieldset (event){
-//     const fieldsetClicked = event.target;
-//     if(fieldsetClicked.classList.contains('hidden')){
-//         fieldsetClicked.classList.remove('hidden');
-//     } else {fieldsetClicked.classList.add('hidden');
-// }}
+const shareOption = () => {
+  if (fieldsetShare.classList.contains('hidden')) {
+    fieldsetDesign.classList.add('hidden');
+    fieldsetFill.classList.add('hidden');
+    fieldsetShare.classList.remove('hidden');
+  } else {
+    fieldsetShare.classList.add('hidden');
+    fieldsetDesign.classList.remove('hidden');
+  }
+};
+const designOption = () => {
+  if (fieldsetDesign.classList.contains('hidden')) {
+    fieldsetDesign.classList.remove('hidden');
+    fieldsetFill.classList.add('hidden');
+    fieldsetShare.classList.add('hidden');
+  } else {
+    fieldsetDesign.classList.add('hidden');
+    fieldsetFill.classList.remove('hidden');
+  }
+};
 
-// option.forEach((oneFieldset) => oneFieldset.addEventListener('click', handleClickFieldset));
+const handleMain = (event) => {
+  const formOption = event.currentTarget;
+  if (formOption === fill) {
+    fillOption();
+  } else if (formOption === share) {
+    shareOption();
+  } else {
+    designOption();
+  }
+};
 
-
-
+mainOption.forEach((option) => {
+  option.addEventListener('click', handleMain);
+});
